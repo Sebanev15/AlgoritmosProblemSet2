d57dbbb918de0ef1dccb1f410e1d9adcd8c902e3
joaquindedios <joaquin.dedios@correo.ucu.edu.uy>
2026-04-26 18:49:05 -0300

    ejercicio 7 parte 1

--- src/main/java/ucu/edu/aed/tda/Main.java
+++ src/main/java/ucu/edu/aed/tda/Main.java
@@ -6,6 +6,22 @@ import java.io.*;
 public class Main {
     public static void main(String[] args){
         //BufferedReader br = new BufferedReader(new FileReader("src\\main\\java\\ucu\\edu\\aed\\tda\\documentos\\consultas.txt"));
-        
+        //ejercicio 7
+            ArbolBinario<String> arbol = new ArbolBinario<>();
+
+        ElementoArbol<String> raiz = new ElementoArbol<>("*");
+
+        raiz.setHijoIzquierdo(new ElementoArbol<>("+"));
+        raiz.setHijoDerecho(new ElementoArbol<>("3"));
+
+        raiz.getHijoIzquierdo().setHijoIzquierdo(new ElementoArbol<>("x"));
+        raiz.getHijoIzquierdo().setHijoDerecho(new ElementoArbol<>("2"));
+        // armo la ecuacion (x+2)*3
+        // cambiando la x por 5
+        //da 21
+        arbol.obtenerRaiz(raiz);
+
+        arbol.sustituirX(5);
+        System.out.println(arbol.evaluar());
     }
 }
