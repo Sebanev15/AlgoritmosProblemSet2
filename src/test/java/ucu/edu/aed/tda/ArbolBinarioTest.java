0f1c240ed84b707f51ed028dc840c3e4c923462e
Sebanev15 <sneves@impulso.edu.uy>
2026-04-29 18:55:13 -0300

    add some tests to arbolBinarioTest

--- src/test/java/ucu/edu/aed/tda/ArbolBinarioTest.java
+++ src/test/java/ucu/edu/aed/tda/ArbolBinarioTest.java
@@ -93,4 +93,229 @@ public class ArbolBinarioTest {
       assertEquals(0,arbol.evaluar());
 
     }
+
+    @Test
+    public void EvaluarExpresionConSuma() {
+        ElementoArbol<String> raiz = new ElementoArbol<>("+");
+        raiz.setHijoIzquierdo(new ElementoArbol<>("5"));
+        raiz.setHijoDerecho(new ElementoArbol<>("3"));
+
+        ArbolBinario<String> arbol = new ArbolBinario<>();
+        arbol.obtenerRaiz(raiz);
+
+        assertEquals(8.0, arbol.evaluar());
+    }
+
+    @Test
+    public void EvaluarExpresionConResta() {
+        ElementoArbol<String> raiz = new ElementoArbol<>("-");
+        raiz.setHijoIzquierdo(new ElementoArbol<>("10"));
+        raiz.setHijoDerecho(new ElementoArbol<>("4"));
+
+        ArbolBinario<String> arbol = new ArbolBinario<>();
+        arbol.obtenerRaiz(raiz);
+
+        assertEquals(6.0, arbol.evaluar());
+    }
+
+    @Test
+    public void EvaluarExpresionConMultiplicacion() {
+        ElementoArbol<String> raiz = new ElementoArbol<>("*");
+        raiz.setHijoIzquierdo(new ElementoArbol<>("6"));
+        raiz.setHijoDerecho(new ElementoArbol<>("7"));
+
+        ArbolBinario<String> arbol = new ArbolBinario<>();
+        arbol.obtenerRaiz(raiz);
+
+        assertEquals(42.0, arbol.evaluar());
+    }
+
+    @Test
+    public void EvaluarExpresionConDivision() {
+        ElementoArbol<String> raiz = new ElementoArbol<>("/");
+        raiz.setHijoIzquierdo(new ElementoArbol<>("20"));
+        raiz.setHijoDerecho(new ElementoArbol<>("4"));
+
+        ArbolBinario<String> arbol = new ArbolBinario<>();
+        arbol.obtenerRaiz(raiz);
+
+        assertEquals(5.0, arbol.evaluar());
+    }
+
+    @Test
+    public void EvaluarExpresionComplejaMultipleOperadores() {
+        ElementoArbol<String> raiz = new ElementoArbol<>("*");
+
+        ElementoArbol<String> sumaIzq = new ElementoArbol<>("+");
+        sumaIzq.setHijoIzquierdo(new ElementoArbol<>("2"));
+        sumaIzq.setHijoDerecho(new ElementoArbol<>("3"));
+
+        ElementoArbol<String> derecha = new ElementoArbol<>("4");
+
+        raiz.setHijoIzquierdo(sumaIzq);
+        raiz.setHijoDerecho(derecha);
+
+        ArbolBinario<String> arbol = new ArbolBinario<>();
+        arbol.obtenerRaiz(raiz);
+
+        assertEquals(20.0, arbol.evaluar());
+    }
+
+    @Test
+    public void EvaluarExpresionConDecimales() {
+        ElementoArbol<String> raiz = new ElementoArbol<>("+");
+        raiz.setHijoIzquierdo(new ElementoArbol<>("2.5"));
+        raiz.setHijoDerecho(new ElementoArbol<>("3.5"));
+
+        ArbolBinario<String> arbol = new ArbolBinario<>();
+        arbol.obtenerRaiz(raiz);
+
+        assertEquals(6.0, arbol.evaluar());
+    }
+
+    @Test
+    public void CompletosEnArbolVacioLanzaExcepcion() {
+        ArbolBinario<String> arbol = new ArbolBinario<>();
+
+        assertThrows(NullPointerException.class, () -> arbol.completos());
+    }
+
+    @Test
+    public void CompletosConSoloRaiz() {
+        ElementoArbol<String> raiz = new ElementoArbol<>("1");
+        ArbolBinario<String> arbol = new ArbolBinario<>();
+        arbol.obtenerRaiz(raiz);
+
+        TDALista<TDAElemento<String>> resultado = arbol.completos();
+
+        assertEquals(0, resultado.tamaño());
+    }
+
+    @Test
+    public void CompletosConMultiplesNodosCompletos() {
+        ElementoArbol<String> raiz = new ElementoArbol<>("1");
+        ElementoArbol<String> izq = new ElementoArbol<>("2");
+        ElementoArbol<String> der = new ElementoArbol<>("3");
+        ElementoArbol<String> izqIzq = new ElementoArbol<>("4");
+        ElementoArbol<String> izqDer = new ElementoArbol<>("5");
+
+        raiz.setHijoIzquierdo(izq);
+        raiz.setHijoDerecho(der);
+        izq.setHijoIzquierdo(izqIzq);
+        izq.setHijoDerecho(izqDer);
+
+        ArbolBinario<String> arbol = new ArbolBinario<>();
+        arbol.obtenerRaiz(raiz);
+
+        TDALista<TDAElemento<String>> resultado = arbol.completos();
+
+        assertTrue(resultado.tamaño() > 0);
+    }
+
+    @Test
+    public void CompletosConArbolSinNodosCompletos() {
+        ElementoArbol<String> raiz = new ElementoArbol<>("1");
+        ElementoArbol<String> izq = new ElementoArbol<>("2");
+        ElementoArbol<String> izqIzq = new ElementoArbol<>("3");
+
+        raiz.setHijoIzquierdo(izq);
+        izq.setHijoIzquierdo(izqIzq);
+
+        ArbolBinario<String> arbol = new ArbolBinario<>();
+        arbol.obtenerRaiz(raiz);
+
+        TDALista<TDAElemento<String>> resultado = arbol.completos();
+
+        assertEquals(0, resultado.tamaño());
+    }
+
+    @Test
+    public void EnNivelEnArbolVacioLanzaExcepcion() {
+        ArbolBinario<String> arbol = new ArbolBinario<>();
+
+        assertThrows(NullPointerException.class, () -> arbol.enNivel(0));
+    }
+
+    @Test
+    public void EnNivelCero() {
+        ElementoArbol<String> raiz = new ElementoArbol<>("1");
+        raiz.setHijoIzquierdo(new ElementoArbol<>("2"));
+        raiz.setHijoDerecho(new ElementoArbol<>("3"));
+
+        ArbolBinario<String> arbol = new ArbolBinario<>();
+        arbol.obtenerRaiz(raiz);
+
+        TDALista<TDAElemento<String>> resultado = arbol.enNivel(0);
+
+        assertEquals(1, resultado.tamaño());
+    }
+
+    @Test
+    public void EnNivelUno() {
+        ElementoArbol<String> raiz = new ElementoArbol<>("1");
+        ElementoArbol<String> izq = new ElementoArbol<>("2");
+        ElementoArbol<String> der = new ElementoArbol<>("3");
+
+        raiz.setHijoIzquierdo(izq);
+        raiz.setHijoDerecho(der);
+
+        ArbolBinario<String> arbol = new ArbolBinario<>();
+        arbol.obtenerRaiz(raiz);
+
+        TDALista<TDAElemento<String>> resultado = arbol.enNivel(1);
+
+        assertEquals(2, resultado.tamaño());
+    }
+
+    @Test
+    public void EnNivelDos() {
+        ElementoArbol<String> raiz = new ElementoArbol<>("1");
+        ElementoArbol<String> izq = new ElementoArbol<>("2");
+        ElementoArbol<String> der = new ElementoArbol<>("3");
+        ElementoArbol<String> izqIzq = new ElementoArbol<>("4");
+        ElementoArbol<String> izqDer = new ElementoArbol<>("5");
+        ElementoArbol<String> derDer = new ElementoArbol<>("6");
+
+        raiz.setHijoIzquierdo(izq);
+        raiz.setHijoDerecho(der);
+        izq.setHijoIzquierdo(izqIzq);
+        izq.setHijoDerecho(izqDer);
+        der.setHijoDerecho(derDer);
+
+        ArbolBinario<String> arbol = new ArbolBinario<>();
+        arbol.obtenerRaiz(raiz);
+
+        TDALista<TDAElemento<String>> resultado = arbol.enNivel(2);
+
+        assertEquals(3, resultado.tamaño());
+    }
+
+    @Test
+    public void EnNivelMayorQueAltura() {
+        ElementoArbol<String> raiz = new ElementoArbol<>("1");
+        ElementoArbol<String> izq = new ElementoArbol<>("2");
+
+        raiz.setHijoIzquierdo(izq);
+
+        ArbolBinario<String> arbol = new ArbolBinario<>();
+        arbol.obtenerRaiz(raiz);
+
+        TDALista<TDAElemento<String>> resultado = arbol.enNivel(5);
+
+        assertTrue(resultado.esVacio());
+    }
+
+    @Test
+    public void EnNivelNegativo() {
+        ElementoArbol<String> raiz = new ElementoArbol<>("1");
+        raiz.setHijoIzquierdo(new ElementoArbol<>("2"));
+
+        ArbolBinario<String> arbol = new ArbolBinario<>();
+        arbol.obtenerRaiz(raiz);
+
+        TDALista<TDAElemento<String>> resultado = arbol.enNivel(-1);
+
+        assertTrue(resultado.esVacio());
+    }
+
 }
